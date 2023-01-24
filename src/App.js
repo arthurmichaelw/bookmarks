f772e2d90041cd6c429bcd455e5544c8ff654bfd
arthurmichaelw <arthurmichaelw@gmail.com>
2023-01-23 20:04:59 -0500

    updated index and app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from ' react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  // Login, SignUp, CreateBookmark, ListBookmarkByUser, DeleteBookmark, UpdateBookmark
+
+const [credentials, setCredentials] = useStat({
+  email: '',
+  password: '',
+  name: ''
+})
+const [bookmark, setBookmark] = useState({
+  title: '',
+  url: ''
+})
+const [bookmarks, setBookmarks] = useState([])
+const [token, setToken] = useState('')
+const login = async () => {
+  try {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email: credentials.email, password: credentials.password })
+  })
+  const tokenResponse = await response.json()
+  setToken(tokenResponse)
+  localStorage.setItem('token', JSON.stringify(tokenResponse))
+ } catch (error) {
+    console.error(error)
+  }
+}
+const signUp = async () => {
+  try {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...credentials })
+  })
+  const tokenResponse = await response.json()
+  setToken(tokenResponse)
+  localStorage.setItem('token', JSON.stringify(tokenResponse))
+ } catch (error) {
+    console.error(error)
+  }
+}
+const createBookmark = async () => {
+  try {
+    const response = await fetch('/api/bookmarks', {
+      method : 'POST',
+      headers: {
+        'Content-Type': 'applicatioin/json',
+        Authorization : `Bearer ${token}`
+      },
+      body : JSON.stringify({...bookmark})
+    })
+    const data = await response.json
+    setBookmarks([data, ...bookmarks])
+    setBookmark({
+      title: '',
+      url: ''
+    })
+
+  } catch (error) {
+      console.error(error)
+  }
+}
+const listBookmarkByUser = async () => {
+  try {
+    const response = await fetch('/api/users/bookmarks')
+    const data = await response.json()
+    setBookmarks(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+const deleteBookmark = async(id) => {
+  try {
+    const response = await fetch(`/api/bookmarks/${id}`, {
+      method: 'DELETE',
+      header: {
+        'Content-Type': 'application.json',
+        Authorization: `Bearer ${token}`
+      }
+    })
+    const data = await response.json()
+    const bookmarksCopy = [...bookmarks]
+    const index = bookmarksCopy.findIndex( bookmark => id === bookmark.id )
+    bookmarksCopy.splice(index, 1)
+    setBookmarks(bookmarksCopy)
+  } catch (error) {
+    console.error(error)
+  }
+}
+const updateBookmark = async(id, updatedData) => {
+  try {
+    const response = await fetch(`/api/bookmarks/${id}`, {
+      method: 'PUT',
+      header: {
+        'Content-Typ': 'application/json',
+        Authorization: `Bearer ${token}`
+    
+      },
+      body: JSON.stringify(updatedData)
+    })
+    const data = await response.json()
+    const bookmarksCopy = [...bookmarks]
+    const index = bookmarksCopy.findIndex( bookmark => id === bookmark.id )
+    bookmarksCopy[index] = {...bookmarksCopy[index], ...updatedData}
+    setBookmarks(bookmarksCopy)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-export default App;
+useEffect(() => {
+  listBookmarkByUser()
+}, [])
+
+  return (
+    <>
+    <ul>
+      { bookmarks.length ? bookmarks.map(item => (
+        <li key={item.id}>
+          <h4>{item.title}</h4>
+          <a href={item.url} target='_blank'>{item.url}</a>
+        </li>
+      )): <>No Bookmarks Added</>}
+    </ul>
+    </>
+  )
+}
